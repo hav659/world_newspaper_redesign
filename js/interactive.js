64547dcbcaf3e5835db2d727ecdd9000cee30ded
Jake Handrick <jake.handrick@gmail.com>
2020-11-17 21:55:44 -0600

    Sticky Nav

--- js/interactive.js
+++ js/interactive.js
@@ -1,14 +1,31 @@
-// NAVBAR DISSAPEAR ON SCROLL
-if ($('.smart-scroll').length > 0) { // check if element exists
-    var last_scroll_top = 0;
-    $(window).on('scroll', function() {
-        scroll_top = $(this).scrollTop();
-        if(scroll_top < last_scroll_top) {
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+      var distanceFromTop = $(document).scrollTop();
+      if (distanceFromTop >= $('.header').height())
+      {
+          $('#sticky').addClass('fixed');
+      }
+      else
+      {
+          $('#sticky').removeClass('fixed');
+      }
+  });
+
+
+console.log("smartScrollLength   ", $('.smart-scroll').length)
+      // NAVBAR DISSAPEAR ON SCROLL
+      if ($('.smart-scroll').length > 0) { // check if element exists
+        var last_scroll_top = 0;
+        $(window).on('scroll', function() {
+          scroll_top = $(this).scrollTop();
+          if (scroll_top < last_scroll_top) {
             $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-        }
-        else {
+          } else {
             $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-        }
-        last_scroll_top = scroll_top;
-    });
-}
+          }
+          last_scroll_top = scroll_top;
+        });
+      }
+
+}); //close readyFunction
